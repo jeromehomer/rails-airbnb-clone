6f53416bcd6e65838703f2114e4ab78f5c98ed37
Pierre-Christophe Callac <pccindo78@msn.com>
2017-02-21 15:20:23 +0100

    added boats with functional index and show methods

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,5 +1,5 @@
 class BoatsController < ApplicationController
-
+  before_action :set_boat, only: [:show, :update, :edit, :distroy]
   # Get /boats
   def index
     @boats = Boat.all
@@ -7,6 +7,31 @@ class BoatsController < ApplicationController
 
   # Get /boats/1
   def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :kind, :address)
+  end
+
+  def set_boat
     @boat = Boat.find(params[:id])
   end
 
