3c0dbb76aadeec02020644cb3f30bb1922043a88
jerome homer <homerjerome@gmail.com>
2017-02-22 16:12:04 +0100

    bootstrap grid on boat index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Boat.create ({
-  name: 'lBeneteau Flyer 550 cabin',
-  address:'Marseille',
-  kind: "Moteur",
-  photos_url: ["https://www.samboat.fr/assets/images/upload/annonce_159/annonce15953d4be2964db0.JPG?d=1406451273", "https://www.samboat.fr/assets/images/upload/annonce_159/annonce15953d4be2964db0.JPG?d=1406451273", "https://www.samboat.fr/assets/images/upload/annonce_159/annonce15955f56f335eb1d.JPG?d=1442148195", "https://www.samboat.fr/assets/images/upload/annonce_159/annonce15955cba5404bcc3.JPG?d=1439409507"]
-  })
-
-
