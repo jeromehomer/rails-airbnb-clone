9282d26c5f9a256983ec084a83bc3bd98e55728f
Antoine Veaux <antoine.veaux@gmail.com>
2017-02-23 10:40:03 +0100

    mail inscription1/2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  after_create :send_welcome_email
+
+
   has_many :boats
   has_many :users
   validates :first_name, :last_name, presence: true
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
+
+
+
 
 end
