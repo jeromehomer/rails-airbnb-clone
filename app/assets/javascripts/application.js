1771c67488eb161b9c98802f4b4a39cd80962e8d
jerome homer <homerjerome@gmail.com>
2017-02-24 12:52:52 +0100

    datepicker wrong

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require bootstrap-datepicker
+//= require jquery.ui.datepicker
+//= require jquery.ui.datepicker-fr
 //= require_tree .
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
